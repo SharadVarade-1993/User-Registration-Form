96b9e5e9a18206a79622532c63f2416113eea1a3
unknown <varadesharad@gmail.com>
2020-04-25 17:16:42 +0530

    again commit because some changes

--- UserRegistrationForm.sh
+++ UserRegistrationForm.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -x
 
-
 shopt -s extglob
-echo "Enter The User Name"
-read user
 echo "user valid Email Address"
-pat3='^[a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$'
-pat4='^([9,1]{2}[ ][0-9]{10})$'
-if [[ $user =~ $pat4 ]]
+read user
+pat='^[a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$'
+#pat='^([9,1]{2}[ ][0-9]{10})$'
+if [[ $user =~ $pat ]]
 then
 		echo valid
 else
 		echo invalid
 fi
+
+echo " "
+echo " "
+
+echo "Enter the mobile number"
+read user
+pat='^([9,1]{2}[ ][0-9]{10})$'
+if [[ $user =~ $pat ]]
+then
+		echo valid
+else
+		echo invalid
+fi
+
